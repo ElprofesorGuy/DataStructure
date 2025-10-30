53c0bf1fa343c7e918fd7b0741e488ba101e9c9d
ElprofesorGuy <guyeinstein15@gmail.com>
2025-10-30 04:34:39 +0100

    "linked list"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 
 void main() {
+    LinkedList list = new LinkedList();
+    list.insertAtStart(13);
+    list.insertAtStart(14);
 
+    list.insert(16);
+    list.insertAt(3,15);
+    list.insert(2);
+    list.insert(51);
+    System.out.println("Liste avant suppression : ");
+    list.displayList();
+    list.delete(4);
+    System.out.println("Liste après suppression : ");
+    list.displayList();
 }
